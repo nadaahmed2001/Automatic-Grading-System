eda9535a1e25ef2a15bf3b2e92c0fbd5153c14c8
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-06-21 19:08:43 +0300

    Update OCR_UsingAI.py

--- AIGradingModel/OCR_Model/OCR_UsingAI.py
+++ AIGradingModel/OCR_Model/OCR_UsingAI.py
@@ -13,6 +13,6 @@ def extract_text_and_ID(file):
   model_2 = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
   student_id  = model_1.generate_content(["write what was written in the first line containing numbers in the photo", file])
-  answer_text = model_2.generate_content(["write text was written in the photo as it without the first line containing only numbers", file])
+  answer_text = model_2.generate_content(["write text was written in the photo as it without the first line containing only numbers,and don't edit any word", file])
 
   return student_id, answer_text
