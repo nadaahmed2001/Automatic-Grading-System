bb4322e9485a901ae1cce40d16a8d63aad2b030e
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-06-24 18:54:57 +0300

    Update OCR_UsingAI.py

--- AIGradingModel/OCR_Model/OCR_UsingAI.py
+++ AIGradingModel/OCR_Model/OCR_UsingAI.py
@@ -4,7 +4,7 @@ import PIL.Image
 GOOGLE_API_KEY = 'AIzaSyAtW0576BvHlrWHvAVguRRYzp_Afm9z7uc'
 genai.configure(api_key=GOOGLE_API_KEY)
 
-def extract_text_and_ID(file,edit='false'):
+def extract_text_and_ID(file, edit='false'):   
 
   file = PIL.Image.open(file)
 
@@ -12,10 +12,11 @@ def extract_text_and_ID(file,edit='false'):
   model_2 = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
   if(edit=='true'):
-    answer_text = model_1.generate_content(file)
+    student_answer = model_1.generate_content(["write text that was written in the area of word ""Answer:"", edit words written incorectly ", file])
   else:
-    answer_text = model_2.generate_content(["write text was written in the photo as it without the first line containing only numbers,and don't edit any word", file])
+    student_answer = model_2.generate_content(["write text that was written in the area of word ""Answer:"", and do not edit any word", file])
 
-  student_id  = model_1.generate_content(["write what was written in the first line containing numbers in the photo", file])
-  
-  return student_id.text,answer_text.text
+  student_id   = model_1.generate_content(["write only numbers was written in the boxes of word ""ID:"" without description", file])
+  student_name = model_1.generate_content(["write only text was written in the box of word ""Name:"" ",file])
+
+  return student_id.text, student_name.text, student_answer.text
