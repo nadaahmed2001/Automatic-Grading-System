1a086467bdec5a50f0e21db0a4d0f9cc064be77d
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-06-29 20:53:23 +0300

    Update OCR_UsingAI.py

--- AIGradingModel/OCR_Model/OCR_UsingAI.py
+++ AIGradingModel/OCR_Model/OCR_UsingAI.py
@@ -1,32 +1,23 @@
 import google.generativeai as genai
 import PIL.Image
-# import os
 
-GOOGLE_API_KEY = 'AIzaSyBESXyRjSDiJTWA9IxeG5bZveutG-j5Y3c'
+GOOGLE_API_KEY = 'AIzaSyA85f_Hc2v-B48TZ3TL4HXKabVBUMYbTkg'
 genai.configure(api_key=GOOGLE_API_KEY)
-# genai.configure(api_key=os.environ['AIzaSyBESXyRjSDiJTWA9IxeG5bZveutG-j5Y3c'])
 
+def OCR_Gemini_Model(file, edit='false'):
 
-def extract_text_and_ID(file, edit='false'):
     file = PIL.Image.open(file)
-    print("File: ", file)
 
     model_1 = genai.GenerativeModel(model_name="gemini-pro-vision")
     model_2 = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
     if edit == 'true':
-        print("I've entered the condition edit = true")
-        student_answer = model_1.generate_content(["extract text that is written in the area of 'Answer:' and correct words that are written incorrectly, give me the extracted text only, don't add anything else", file])
-        print("Student answer from model 1: ", student_answer.text)
+        student_answer = model_1.generate_content(["extract text that is written in the area of 'Answer:' and correct words that are written incorrectly,without description", file])
     else:
-        print("I've entered the condition edit = false")
-        student_answer = model_2.generate_content(["extract text that is written in the area of 'Answer:' and don't correct words that are written incorrectly , give me the extracted text only, don't add anything else", file])
-        print("Student answer from model 2: ", student_answer.text)
+        student_answer = model_2.generate_content(["extract text that is written in the area of 'Answer:' and don't correct words that are written incorrectly , give me the extracted text only,without description", file])
 
-    student_id = model_1.generate_content(["write only numbers was written in the boxes of word 'ID:' without description", file])
-    student_name = model_1.generate_content(["write only text was written in the box of word 'Name:'", file])
+    student_id = model_1.generate_content(["write only numbers was written in the area of word 'ID:' without description", file])
+    student_name= model_1.generate_content(["write only text was written in the area of word 'Name:' without description", file])
 
-    print("Student ID: ", student_id.text)
-    print("Student Name: ", student_name.text)
 
-    return student_id.text, student_name.text, student_answer.text
+    return student_id.text.replace(' ',''), student_name.text, student_answer.text
