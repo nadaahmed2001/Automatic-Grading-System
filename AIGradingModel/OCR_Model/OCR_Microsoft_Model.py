0aed49097d2725051bda8764ee76992507b294a5
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-07-01 13:03:38 +0300

    Update OCR_Microsoft_Model.py

--- AIGradingModel/OCR_Model/OCR_Microsoft_Model.py
+++ AIGradingModel/OCR_Model/OCR_Microsoft_Model.py
@@ -127,7 +127,7 @@ def ocr_space_file(file, overlay=False, api_key='1b70baf52f88957', language='eng
 
         return r.content.decode()
 
-def OCR_Microsoft_Model(file):
+def OCR_Microsoft_Model(file, processor, model):
     images = Segment_exam_Paper(file)
 
     extracted_ID   = json.loads(ocr_space_file(file = Image.fromarray(images[0]).convert("RGB"), language='eng'))
@@ -139,7 +139,7 @@ def OCR_Microsoft_Model(file):
     student_Name = extracted_Name["ParsedResults"][0]["ParsedText"]
     student_Name   = student_Name.replace('Name:', '')
 
-    processor, model = load_model()
+    #processor, model = load_model()
 
     student_answer = extract_student_answer(images, processor, model)
 
