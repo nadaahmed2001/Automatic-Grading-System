16fd74461b6a78dab674b114053600faf73f9149
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-07-04 21:11:15 +0300

    Update OCR_Space_Model.py

--- AIGradingModel/OCR_Model/OCR_Space_Model.py
+++ AIGradingModel/OCR_Model/OCR_Space_Model.py
@@ -23,13 +23,14 @@ def OCR_Space_Model(file):
         extracted_text = ocr_space_file(file=file)
 
         response_data = json.loads(extracted_text)
+    
         extracted_text_concatenated = response_data["ParsedResults"][0]["ParsedText"] if "ParsedResults" in response_data else ""
 
         lines = extracted_text_concatenated.split('\n')
-
         
         student_ID = lines[0].replace('ID', '').replace('1D','').replace(' ', '').replace('\n', '')
         student_Name = lines[1].replace('Name', '')
+    
         student_answer = lines[2:]
         student_answer = [answer.strip() for answer in student_answer if answer.strip()]
         student_answer = '\n'.join(student_answer)
