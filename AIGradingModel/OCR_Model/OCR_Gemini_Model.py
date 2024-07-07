e301a2eb01b071ba93dbac9dd6f1ede5c603cbaf
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-07-07 20:41:59 +0300

    Update OCR_Gemini_Model.py

--- AIGradingModel/OCR_Model/OCR_Gemini_Model.py
+++ AIGradingModel/OCR_Model/OCR_Gemini_Model.py
@@ -1,24 +1,52 @@
 import google.generativeai as genai
 import PIL.Image
+import io
+from pathlib import Path
+from pypdf import PdfReader
+import PIL.Image
+import io
+import re
 
-GOOGLE_API_KEY = 'AIzaSyCub9APQCFZ0wP3ggQ0qMq96dS2cVB3CuM'
-# GOOGLE_API_KEY ='' #To test error handling
+
+GOOGLE_API_KEY = 'AIzaSyA85f_Hc2v-B48TZ3TL4HXKabVBUMYbTkg'
 genai.configure(api_key=GOOGLE_API_KEY)
 
-def OCR_Gemini_Model(file, edit):
+# !pip install pypdf
 
-    file = PIL.Image.open(file)
-
-    model_1 = genai.GenerativeModel(model_name="gemini-pro-vision")
-    model_2 = genai.GenerativeModel(model_name="gemini-1.5-flash")
-
-    if edit == 'true':
-        student_answer = model_1.generate_content(["extract text that is written in the area of 'Answer:' and correct words that are written incorrectly,without description", file])
+def check_file_extension(file_path):
+    file_extension = Path(file_path).suffix.lower()
+    if file_extension == '.pdf':
+        return "PDF"
+    elif file_extension in ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff']:
+        return "image"
     else:
-        student_answer = model_2.generate_content(["extract text that is written in the area of 'Answer:' and don't correct words that are written incorrectly , give me the extracted text only,without description", file])
+        return "Unknown file type."
 
-    student_id = model_1.generate_content(["write only numbers was written in the area of word 'ID:' without description", file])
-    student_name= model_1.generate_content(["write only text was written in the area of word 'Name:' without description", file])
+def extract_text_and_ID(file,edit='false'):
+  
+  model_1 = genai.GenerativeModel(model_name="gemini-pro-vision")
+  model_2 = genai.GenerativeModel(model_name="gemini-1.5-flash")
+
+  if(check_file_extension(file)=="PDF"):
+        reader = PdfReader(file)
+        page = reader.pages[0]
+        image_bytes = page.images[0].data 
+
+        image = PIL.Image.open(io.BytesIO(image_bytes))
+
+        file = image 
+  else:
+      file = PIL.Image.open(file)  # Pass the file path, not the image object
+
+  if(edit=='true'):
+    # Pass the image object to the model, not the file path
+    answer_text = model_1.generate_content(["write text was written in the area of word ""Answer"", edit words written incorectly ",file])
+  else:
+    answer_text = model_2.generate_content(["write text was written in the area of word ""Answer"", and do not edit any word", file]) # Pass the image object to the model, not the file path
+
+  student_id   = model_2.generate_content(["write only numbers was written in the area of word ""ID"" without description", file]) # Pass the image object to the model, not the file path
+  student_name = model_2.generate_content(["write only text was written in the area of word ""Name"" without description", file]) # Pass the image object to the model, not the file path
 
 
-    return student_id.text.replace(' ',''), student_name.text, student_answer.text
+
+  return student_id.text, student_name.text, answer_text.text.replace('Answer','')
