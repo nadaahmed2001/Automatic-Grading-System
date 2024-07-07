509ec12c335b4584746001010f73d7ff0c45ee87
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-07-07 20:12:56 +0300

    Update OCR_Space_Model.py to take image as pdf extension

--- AIGradingModel/OCR_Model/OCR_Space_Model.py
+++ AIGradingModel/OCR_Model/OCR_Space_Model.py
@@ -1,7 +1,20 @@
 import requests
 import json
 import os
+from pathlib import Path
+from pypdf import PdfReader
+import PIL.Image
+import io
+import re
 
+def check_file_extension(file_path):
+    file_extension = Path(file_path).suffix.lower()
+    if file_extension == '.pdf':
+        return "PDF"
+    elif file_extension in ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff']:
+        return "image"
+    else:
+        return "Unknown file type."
 
 def ocr_space_file(file, overlay=False, api_key='K88613798688957', language='eng'):
 # def ocr_space_file(file, overlay=False, api_key='', language='eng'):
@@ -21,7 +34,20 @@ def ocr_space_file(file, overlay=False, api_key='K88613798688957', language='eng
     return r.content.decode()
 
 def OCR_Space_Model(file):
-    extracted_text_json = ocr_space_file(file)
+    if(check_file_extension(file)=="PDF"):
+        reader = PdfReader(file)
+        page = reader.pages[0]
+        image_bytes = page.images[0].data 
+
+        image = PIL.Image.open(io.BytesIO(image_bytes))
+
+        temp_image_path = "/tmp/temp_image.jpg" 
+        image.save(temp_image_path)
+        file_path = temp_image_path 
+    else:
+        file_path = file
+
+    extracted_text_json = ocr_space_file(file_path)
     response_data = json.loads(extracted_text_json)
     
     text = response_data["ParsedResults"][0]["ParsedText"] if "ParsedResults" in response_data else ""
@@ -37,3 +63,8 @@ def OCR_Space_Model(file):
         student_id, student_name, answer_text = '', ''
 
     return student_id, student_name ,answer_text
+
+student_id, student_name ,answer_text = OCR_Space_Model(file= "/content/answer as pdf.pdf")
+print(student_id)
+print(student_name)
+print(answer_text)
