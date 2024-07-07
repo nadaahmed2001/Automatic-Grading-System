ba58f5921b8effd9160ed442760ae93fcd808c7b
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-07-07 20:02:55 +0300

    Update OCR_Verify_Model.py

--- AIGradingModel/OCR_Model/OCR_Verify_Model.py
+++ AIGradingModel/OCR_Model/OCR_Verify_Model.py
@@ -1,40 +1,57 @@
 import veryfi
 from veryfi import Client
+from pathlib import Path
+from pypdf import PdfReader
+import PIL.Image
+import io
+import re
+
+def check_file_extension(file_path):
+    file_extension = Path(file_path).suffix.lower()
+    if file_extension == '.pdf':
+        return "PDF"
+    elif file_extension in ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff']:
+        return "image"
+    else:
+        return "Unknown file type."
 
 def OCR_Verify_Model(file):
     client_id = 'vrf6ndisxpgBtTaPCAVHwnecpJUn4ci9oAyOse7'
     client_secret = 'zPIFugSt5cOlCCXURPbyS101uxIiiINDDMNx83Ygwf61gyUie5sfGr7OLXYagZiixwnGrWojl1TnLi0dAlA7d2kIkrlvdKyci2Ye3HEgb8kYk6DiY7f63Ybr7pRHooyR'
     username = 'hadeer.fcai'
     api_key = '49bb60a0248602c1517f4925a6786a08'
-    # api_key = ''
 
     categories = ['Grocery', 'Utilities', 'Travel']
-    file_path = file
+
+    if(check_file_extension(file)=="PDF"):
+        reader = PdfReader(file)
+        page = reader.pages[0]
+        image_bytes = page.images[0].data 
+
+        image = PIL.Image.open(io.BytesIO(image_bytes))
+
+        temp_image_path = "/tmp/temp_image.jpg" 
+        image.save(temp_image_path)
+        file_path = temp_image_path 
+    else:
+        file_path = file
+
     veryfi_client = Client(client_id, client_secret, username, api_key)
     response = veryfi_client.process_document(file_path, categories=categories)
-    # Accessing and printing the ocr_text with corrected spacing
+
     ocr_text = response['ocr_text']
+    
+    # Replace multiple spaces with a single space
+    ocr_text = re.sub(r'\s+', ' ', ocr_text)
+    
     lines = ocr_text.split('\n')
-    student_id = ""
-    student_name = ""
-    student_answer = ""
 
-    for i, line in enumerate(lines):
-        stripped_line = line.replace(' ', '').strip()
-        if 'ID' in line:
-            # Check if ID is on the same line
-            id_line = line.replace('ID', '').strip()
-            student_id = id_line
-            # Check if ID is on the previous line
-            if i > 0:
-                prev_line = lines[i - 1].replace(' ', '').strip()
-                if prev_line.isdigit():
-                    student_id = lines[i - 1].strip()
-        if 'Name' in line:
-            student_name = line.split('Name', 1)[1].strip()
-        if 'Answer' in line:
-            student_answer = line.split('Answer', 1)[1].strip()
-            continue
-        student_answer += " " +line.strip() + "\n"
+    student_id = response.get('invoice_number','')
+      
+    answer_start = ocr_text.find('Answer')
+    name_start = ocr_text.find('Name') + len('Name')
 
-    return student_id.replace(' ',''), student_name, student_answer
+    student_name = ocr_text[name_start:answer_start].strip()
+    student_answer = ocr_text[answer_start:].strip().replace('Answer','')
+
+    return student_id, student_name, student_answer
